015574b12172d144c139ac1f9396e65ba1eda25f
shazaaly <shaza.aly@gmail.com>
2024-02-08 15:49:31 +0200

    Refactor password validation function***

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""_summary_
+"""
 """
 
 import bcrypt
@@ -18,3 +18,18 @@ def hash_password(password: str) -> bytes:
     byt_psd = password.encode()
     salted = bcrypt.hashpw(byt_psd, bcrypt.gensalt())
     return salted
+
+
+def is_valid(password, hashed_password) -> bool:
+    """_summary_
+
+    Args:
+        password (_type_): _description_
+        hashed_password (bool): _description_
+
+    Returns:
+        bool: _description_
+    """
+
+    encoded = bcrypt.encode(password)
+    return bcrypt.checkpw(encoded, hashed_password)
