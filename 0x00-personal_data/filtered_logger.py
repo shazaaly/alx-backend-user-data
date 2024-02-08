9b2245eaa69ebf45a0117919d52429aaca6df535
shazaaly <shaza.aly@gmail.com>
2024-02-08 14:54:02 +0200

    Add main entry point to filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,7 @@ Example usage:
 """
 
 from ast import List
+from dataclasses import field
 import re
 import os
 import mysql.connector
@@ -99,3 +100,23 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
                                    host=host,
                                    database=db_name)
     return conn
+
+
+def main():
+    """
+    main entry point
+    """
+    db = get_db()
+    logger = get_logger()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    fields = cursor.column_names
+    for row in cursor:
+        message = "".join("{}={}; ".format(k, v) for k, v in zip(fields, row))
+        logger.info(message.strip())
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
