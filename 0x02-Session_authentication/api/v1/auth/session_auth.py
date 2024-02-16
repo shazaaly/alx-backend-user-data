6cc6fde726918ce88717b17d945349b0ac9e73c8
shazaaly <shaza.aly@gmail.com>
2024-02-17 01:34:20 +0200

    Add session authentication functionality

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,8 @@
 """
 
 from .auth import Auth
+from flask import session
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
@@ -11,3 +13,14 @@ class SessionAuth(Auth):
     Args:
         Auth (_type_): _description_
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        else:
+            id = uuid4()
+            self.user_id_by_session_id[str(id)] = user_id
+            return str(id)
